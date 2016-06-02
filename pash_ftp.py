6c746764ffb618f8696d340dc59b703974a34e57
Mark <MarkPash@users.noreply.github.com>
2016-06-02 18:26:21 +0100

    Bit of a rewrite that allows for init without vars and connection and download via instance method + cleanup

--- pash_ftp.py
+++ pash_ftp.py
@@ -5,19 +5,22 @@ import shutil
 
 
 class Downloader:
+    def __init__(self, ftp_server = '', ftp_user = '', ftp_password = ''):
+        if ftp_server is not '':
+            self.connect(ftp_server, ftp_user, ftp_password)
 
-    def __init__(self, server, username, password, ftp_directory, ftp_file, threads):
-        self.server = server
-        self.username = username
-        self.password = password
-        self.directory = ftp_directory
-        self.ftp = ftplib.FTP(self.server, self.username, self.password)
-        self.file = ftp_file
-        self.file_path = self.directory + '/' + self.file
+    def connect(self, ftp_server, ftp_user, ftp_password = ''):
+        self.ftp_server = ftp_server
+        self.ftp_user = ftp_user
+        self.ftp_password = ftp_password
+        self.ftp = ftplib.FTP(self.ftp_server, self.ftp_user, self.ftp_password)
+
+    def download(self, ftp_file_path, threads):
+        self.ftp_file_path = ftp_file_path
         self.parts = threads
-        self.file_size = self.ftp.size(self.file_path)
-        self.chunk_size = self.file_size/self.parts
-        self.last_chunk_size = self.file_size - (self.chunk_size * (self.parts - 1))
+        self.ftp_file_size = self.ftp.size(self.ftp_file_path)
+        self.chunk_size = self.ftp_file_size/self.parts
+        self.last_chunk_size = self.ftp_file_size - (self.chunk_size * (self.parts - 1))
 
         partdownloaders = []
         for part in range(self.parts):
@@ -25,10 +28,11 @@ class Downloader:
                 this_chunk_size = self.last_chunk_size
             else:
                 this_chunk_size = self.chunk_size
-            partdownloaders.append(DownloadPart(self.server, self.username, self.password, self.directory, self.file, part, self.chunk_size * part, this_chunk_size))
+            ftp = ftplib.FTP(self.ftp_server, self.ftp_user, self.ftp_password)
+            partdownloaders.append(DownloadPart(ftp, self.ftp_file_path, part, self.chunk_size * part, this_chunk_size))
         for part in partdownloaders:
             part.thread.join()
-        with open(self.file, 'w+b') as f:
+        with open(os.path.basename(self.ftp_file_path), 'w+b') as f:
             for downloader in partdownloaders:
                 shutil.copyfileobj(open(downloader.part_name, 'rb'), f)
         for part in partdownloaders:
@@ -36,25 +40,20 @@ class Downloader:
 
 
 class DownloadPart:
-
-    def __init__(self, server, username, password, ftp_directory, ftp_file, part_number, part_start, part_size):
-        self.server = server
-        self.username = username
-        self.password = password
-        self.directory = ftp_directory
-        self.ftp = ftplib.FTP(self.server, self.username, self.password)
-        self.file = ftp_file
-        self.file_path = self.directory + '/' + self.file
+    def __init__(self, ftp, ftp_file_path, part_number, part_start, part_size):
+        self.ftp = ftp
+        self.ftp_file_path = ftp_file_path
+        self.ftp_file = os.path.basename(self.ftp_file_path)
         self.part_number = part_number
         self.part_start = part_start
         self.part_size = part_size
-        self.part_name = self.file + '.part' + str(self.part_number)
+        self.part_name = self.ftp_file + '.part' + str(self.part_number)
         self.thread = threading.Thread(target=self.receive_thread)
         self.thread.start()
 
     def receive_thread(self):
         try:
-            self.ftp.retrbinary('RETR ' + self.file_path, self.on_data, 100000, self.part_start)
+            self.ftp.retrbinary('RETR ' + self.ftp_file_path, self.on_data, 100000, self.part_start)
         except:
             pass
 
@@ -65,6 +64,3 @@ class DownloadPart:
             with open(self.part_name, 'r+b') as f:
                 f.truncate(self.part_size)
             raise
-
-
-Downloader('server', 'user', 'password', 'path', 'file', 20)
